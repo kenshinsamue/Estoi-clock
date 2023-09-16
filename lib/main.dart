dc6531f073491e535236f28ed418e0a1280a693f
epedroza <epedroza@integracanarias.com>
2023-09-16 19:17:50 +0100

    hotfix para la carga

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:estoi_clock/configurationDB.dart';
 import 'package:estoi_clock/dbConnection.dart';
 import 'package:estoi_clock/notificationService.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:permission_handler/permission_handler.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initNotifications();
@@ -36,13 +38,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late DatabaseService _appSettings ;
+  DatabaseService _appSettings = AppConfiguration();
   final ReceivePort  receivePort = ReceivePort();
   late Isolate isolate;
   Capability? capability;
   int result = 0;
   late dynamic status;
-
+  bool settingState = false;
   @override
   void initState() {
     super.initState();
@@ -52,101 +54,167 @@ class _MyHomePageState extends State<MyHomePage> {
     _appSettings = AppConfiguration();
   }
 
-  Future init()  {
+  Future init() async {
     status =  Permission.notification.request();
-    return _appSettings.selectWhere("configurations = 'habilitado'");
+    dynamic result = await _appSettings.selectWhere("configurations = 'habilitado'");
+    return result;
   }
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: init(),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-          if(snapshot.hasData){
-            bool settingState = (snapshot.data[0]["valor"]=="true")?true:false;
-            if(settingState){
-              showNotification();
-            }
-            return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).primaryColor,
-                title: const Text("EstoiClock"),
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: const Text("EstoiClock"),
+        ),
+        body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.04),
+                child: Row(
                   children: [
-                    Padding(
-                      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.04),
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.75,
-                            child: const Text("Habilitar EstoiClock para que me ayude a gestionar mi tiempo"),
-                          ),
-
-                          Switch(
-                            value:settingState,
-                            onChanged: (value) async  {
-
-                              if(mounted){
-                                hideNotification();
-                                isolate = await isolateFunc(receivePort);
-
-                                if(value == false){
-                                  if(capability == null) {
-                                    capability = isolate.pause(isolate.pauseCapability);
-                                  } else {
-                                    capability = isolate.pause(capability);
-                                  }
-                                }
-                                if (value == true){
-                                  if(capability != null) {
-                                    isolate.resume(capability!);
-                                  }
-                                  else{
-                                    isolate = await isolateFunc(receivePort);
-                                  }
-                                  showNotification();
-                                }
-                                setState(() {
-                                  settingState = value;
-                                  if (snapshot.hasData) {
-                                    final int id = snapshot.data[0]["id"];
-                                    final String newValue = value ? "true" : "false";
-                                    _appSettings.updateData({'valor': newValue}, "id = $id");
-                                  }
-                                });
-                              }
-                            },
-                          ),
-                        ],
-                      ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.75,
+                      child: const Text("Habilitar EstoiClock para que me ayude a gestionar mi tiempo"),
                     ),
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: ListTile(
-                          title: const Text("Aplicaciones Instaladas"),
-                          subtitle:const  Text(
-                              "Seleccione las aplicaciones que van a ser monitorizadas"),
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => InstalledAppsScreen(),
-                            ),
-                          ),
-                        ),
-                      ),
+
+                    Switch(
+                      value:settingState,
+                      onChanged: (value) async  {
+
+                        if(mounted){
+                          hideNotification();
+                          isolate = await isolateFunc(receivePort);
+
+                          if(value == false){
+                            if(capability == null) {
+                              capability = isolate.pause(isolate.pauseCapability);
+                            } else {
+                              capability = isolate.pause(capability);
+                            }
+                          }
+                          if (value == true){
+                            if(capability != null) {
+                              isolate.resume(capability!);
+                            }
+                            else{
+                              isolate = await isolateFunc(receivePort);
+                            }
+                            showNotification();
+                          }
+                          setState(() {
+                            settingState = value;
+                          });
+                        }
+                      },
                     ),
                   ],
                 ),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-            );
-          }
-          else{
-            return Container();
-          }
-        }
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ListTile(
+                    title: const Text("Aplicaciones Instaladas"),
+                    subtitle:const  Text(
+                        "Seleccione las aplicaciones que van a ser monitorizadas"),
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => InstalledAppsScreen(),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ) //
+
+        // FutureBuilder(
+        //   future: init(),
+        //   initialData: const [{"id": 1, "configurations": "habilitado", "valor": false,"default":true}],
+        //   builder: (BuildContext context, AsyncSnapshot snapshot){
+        //       print (snapshot.data);
+        //       if(snapshot.connectionState == ConnectionState.done ){
+        //         print (snapshot.data);
+        //         bool settingState = (snapshot.data[0]["valor"]=="true")?true:false;
+        //         if(settingState){
+        //           showNotification();
+        //         }
+        //         return Center(
+        //             child: Column(
+        //               mainAxisAlignment: MainAxisAlignment.start,
+        //               children: [
+        //                 Padding(
+        //                   padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.04),
+        //                   child: Row(
+        //                     children: [
+        //                       SizedBox(
+        //                         width: MediaQuery.of(context).size.width * 0.75,
+        //                         child: const Text("Habilitar EstoiClock para que me ayude a gestionar mi tiempo"),
+        //                       ),
+        //
+        //                       Switch(
+        //                         value:settingState,
+        //                         onChanged: (value) async  {
+        //
+        //                           if(mounted){
+        //                             hideNotification();
+        //                             isolate = await isolateFunc(receivePort);
+        //
+        //                             if(value == false){
+        //                               if(capability == null) {
+        //                                 capability = isolate.pause(isolate.pauseCapability);
+        //                               } else {
+        //                                 capability = isolate.pause(capability);
+        //                               }
+        //                             }
+        //                             if (value == true){
+        //                               if(capability != null) {
+        //                                 isolate.resume(capability!);
+        //                               }
+        //                               else{
+        //                                 isolate = await isolateFunc(receivePort);
+        //                               }
+        //                               showNotification();
+        //                             }
+        //                             setState(() {
+        //                               settingState = value;
+        //                             });
+        //                           }
+        //                         },
+        //                       ),
+        //                     ],
+        //                   ),
+        //                 ),
+        //                 Card(
+        //                   child: Padding(
+        //                     padding: const EdgeInsets.all(8),
+        //                     child: ListTile(
+        //                       title: const Text("Aplicaciones Instaladas"),
+        //                       subtitle:const  Text(
+        //                           "Seleccione las aplicaciones que van a ser monitorizadas"),
+        //                       onTap: () => Navigator.push(
+        //                         context,
+        //                         MaterialPageRoute(
+        //                           builder: (context) => InstalledAppsScreen(),
+        //                         ),
+        //                       ),
+        //                     ),
+        //                   ),
+        //                 ),
+        //               ],
+        //             ),
+        //           ); // This trailing comma makes auto-formatting nicer for build methods.
+        //       }
+        //
+        //       else{
+        //         return CircularProgressIndicator();
+        //       }
+        //     }
+        // )
     );
   }
 }
@@ -157,6 +225,5 @@ class _MyHomePageState extends State<MyHomePage> {
 
   int runTask(SendPort arg){
     SendPort resultPort = arg;
-    print ("esta funcionando");
     Isolate.exit(resultPort,1);
   }
